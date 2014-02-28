2a121feb96e8146b27cd72b2df96b9b2fa95fe3e
mousterian <rivladi@gmail.com>
2014-02-27 20:57:04 -0800

    added a semi-working version of hovering over

--- Javascript_jquery/js/script.js
+++ Javascript_jquery/js/script.js
@@ -1,5 +1,4 @@
-
-
+//var dims = new Array();
 $(document).ready(function()
 {
     //$('button').click(function() {
@@ -8,18 +7,61 @@ $(document).ready(function()
     //});
 
     // Event binding using a convenience method
-    $( "#add" ).click(function( event ) {
-        alert( "Hello." );
+
+    $( "#add" ).click(function( event )
+    {
+        //location.reload(false);
+        //$(document).removeClass(".grid");
+
+
+        var input = prompt("Please select how many grids to display, \n but no more than 64!");
+        $head = $(".header");
+        var count = input;//test();
+        var dimensions = (960 - count*2) / count;
+
+
+
+        for(var i = 0; i < count * count; i++)
+        {
+           // $grd = createDivs(dimensions);
+            //$grd.css("background-color", "red");
+            //$dims[i] =
+            createDivs(dimensions,i);
+        }
+
+        $('.grid').hover(function()
+        {
+
+            for(var j = 0; j < count * count; j++)
+            {
+
+                //$(this).attr('id').hover(function()
+                //{
+                    $(this).text($(this).attr('id')).css("background-color", "blue");
+                //});
+
+
+            }
+
+        });
+
+
+
+
+
     });
 
-    $head = $(".header");
-    var count = test();
-    var dimensions = (960 - count*2) / count;
+       $('#1').hover(function(){
+    //     for (var i = 0; i < dims.length, i++){
+    //         $d = dims[i];
+             $('#1').css("background-color", "blue");
+    //     }
 
-    for(var i = 0; i < count * count; i++)
-    {
-        createDivs(dimensions);
-    }
+     });
+
+
+
+
 
 });
 
@@ -28,9 +70,15 @@ function test(){
     return count;
 }
 
-function createDivs(size){
-    d=document.createElement('div');
+function createDivs(size,ids){
+
+    d = document.createElement('div');
+    $(d).addClass("grid");
+    //$(d).attr("id",id.toString());
     $divgrid = $(d);
+    var idToString = ids.toString();
+    $divgrid.attr("id",idToString);
+
     $('.wrapper').append($divgrid);
 
     $divgrid.css("background-color","purple");
@@ -43,7 +91,9 @@ function createDivs(size){
     $divgrid.height(h);
     $divgrid.width(h);
 
+    //return $divgrid;
 
+    //return $divgrid;
 }
 
 
