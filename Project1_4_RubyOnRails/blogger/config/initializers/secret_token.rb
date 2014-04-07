2a74312314fd36d35c67e0f9ab5af9dff9a3714e
mousterian <rivladi@gmail.com>
2014-04-06 20:58:10 -0700

    initiated new rails template

---
+++ Project1_4_RubyOnRails/blogger/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '978b839d801149aecd8b91a2f4f2c702f88c9a356ec8907bb65f56d59c43f6b5784034ca938c9bfc9b037280cb6f73b755813b5c0f0940d369e40a2746bb6cf8'
