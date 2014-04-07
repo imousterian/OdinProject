0cf93e8ffec245d08582eeb0dbf281c971b8d31d
mousterian <rivladi@gmail.com>
2014-04-07 15:04:15 -0700

    tagging feature added

--- Project1_4_RubyOnRails/blogger/app/controllers/comments_controller.rb
+++ Project1_4_RubyOnRails/blogger/app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
     include CommentsHelper
+
     def create
         @comment = Comment.new(comment_params)
         @comment.article_id = params[:article_id]
