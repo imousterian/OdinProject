1f7460a967bb0eadd1a4f86216ac0a3282991a38
mousterian <rivladi@gmail.com>
2014-04-18 18:09:55 -0700

    cleaned up caesar cipher

--- Project2_1_Ruby_Basic/CaesarCipher.rb
+++ Project2_1_Ruby_Basic/CaesarCipher.rb
@@ -1,5 +1,42 @@
-def caesar_cipher(str,num)
-    puts str
+#implementation of caesar cipher method using hashes
+
+def caesar_cipher(arr,num=0)
+
+    hash = Hash[('a'..'z').map.with_index.to_a]
+
+    (0..arr.length-1).each do |item|
+
+        letter = arr[item].downcase
+
+        if (hash[letter] != nil)
+
+            new_index = hash[letter] + num.to_i
+
+            if (new_index >= 26)
+                new_index = new_index - 26
+            end
+
+            new_value = hash.key(new_index)
+
+            if (arr[item] === arr[item].capitalize)
+                new_value = new_value.upcase
+            end
+
+            arr[item] = new_value
+
+        end
+    end
+
+    return arr
+
 end
 
-caesar_cipher("What a string!", 5)
+puts "Enter a string to cipher: "
+input = gets.chomp
+puts "Enter number of letters to shift: "
+number = gets.chomp
+puts caesar_cipher(input, number)
+
+
+
+
