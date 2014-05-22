469f08610ea254f01c1bb28b71fe6459c3d50e1f
mousterian <rivladi@gmail.com>
2014-05-21 21:28:37 -0700

    initial commit of re-former

---
+++ Project3_Forms/re-former/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReFormer::Application.config.secret_key_base = '827d323359e81d34239ee8f66f3cc3ebcc29e8c95081b79b370cfbbcea9f3c70122485137915d5a7ecad4c2d563081ae18f6366c71c2cdde1368f6ce65cc879c'
