3796083524ccefbbbba02c3b6c64144964b91a36
mousterian <rivladi@gmail.com>
2014-05-22 22:28:48 -0700

    updated forms

--- Project3_Forms/re-former/app/controllers/users_controller.rb
+++ Project3_Forms/re-former/app/controllers/users_controller.rb
@@ -13,6 +13,21 @@ class UsersController < ApplicationController
         end
     end
 
+    def edit
+        @user = User.find(params[:id])
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            flash[:success] = "Profile updated."
+            redirect_to edit_user_path(@user)
+        else
+            flash.now[:error] = "Oops."
+            render :edit
+        end
+  end
+
     private
         def user_params
             params.require(:user).permit(:username,:email,:password)
--- Project3_Forms/re-former/config/routes.rb
+++ Project3_Forms/re-former/config/routes.rb
@@ -1,6 +1,6 @@
 ReFormer::Application.routes.draw do
 
-    resources :users, only: [:new, :create]
+    resources :users, only: [:create, :new, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
