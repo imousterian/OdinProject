50f267daf1778e5d702510345c25f78bf38ceb4f
mousterian <rivladi@gmail.com>
2014-06-27 18:55:28 -0700

    minor cleanup

--- Project3_Authentication/members-only/app/controllers/posts_controller.rb
+++ Project3_Authentication/members-only/app/controllers/posts_controller.rb
@@ -3,14 +3,12 @@ class PostsController < ApplicationController
     before_action :restrict_to_signed_in, only: [:new, :create]
 
     def new
-        # @post = current_user.posts.build
         @post = Post.new
     end
 
     def create
         @post = current_user.posts.build(post_params)
         if @post.save
-            # redirect_to current_user #root_url
             redirect_to root_path
         end
     end
