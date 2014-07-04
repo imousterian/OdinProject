428518d155a047213b272816dab679144ebc1c27
mousterian <rivladi@gmail.com>
2014-07-04 01:19:39 -0700

    implemented simple server and browser

--- Project2_5_Ruby_Web/webserver/browser.rb
+++ Project2_5_Ruby_Web/webserver/browser.rb
@@ -1,19 +1,33 @@
 require 'socket'
+require 'json'
 
 
 host = 'localhost'
 port =  2000
-path = "/index.htm"
 
 #HTTP request to send to fetch a file
-# request = "GET #{path} HTTP/1.0\r\n\r\n"
-#request = "GET /index.html HTTP/1.0\r\n\r\n"
+
 
 puts "Please select GET or POST"
-if "GET"
+input = gets.chomp
+
+if input == "GET"
+
     request = "GET /index.html HTTP/1.0\r\n\r\n"
-else
-    request = "POST /thanks.html HTTP/1.0\r\n\r\n"
+
+elsif input == "POST"
+
+    puts "Enter your name:"
+    name = gets.chomp
+    puts "Enter your email:"
+    email = gets.chomp
+
+    hash_to_send = {:viking => {:name => name, :email => email} }
+
+    hash_to_json = hash_to_send.to_json
+
+    request = "POST /thanks.html HTTP/1.0\r\nContent-Length: #{hash_to_json.size}\r\n\r\n#{hash_to_json}"
+
 end
 
 
@@ -26,3 +40,4 @@ response = socket.read
 #split response at first blank line into headers and body
 headers,body = response.split("\r\n\r\n", 2)
 print body
+socket.close
--- Project2_5_Ruby_Web/webserver/server.rb
+++ Project2_5_Ruby_Web/webserver/server.rb
@@ -1,40 +1,40 @@
 require 'socket'
-
+require 'json'
 
 server = TCPServer.open(2000)
 
 loop{
 
-    client = server.accept
+    Thread.start(server.accept) do |client|
 
-    request = client.gets
+        request = client.read_nonblock(256)
 
-    Thread.start(client) do |client|
-        # request = client.read_nonblock(256) #cllient.gets
         header,body = request.split("\r\n\r\n", 2)
-        # fileName = request.gsub(/GET /, '').gsub(/ HTTP.*/, '')
-        fileName = request.match(/(\w+\/)*\w+\.\w+/)[0]
-        http_method = header
-        # puts request
-        puts http_method
+
+        fileName = header.split[1][1..-1]
 
         if File.exist?(fileName)
-            print "HTTP/1.1 200/OK\r\nServer: ABC-server\r\nContent-type:text/html\r\n\r\n"
 
             file = File.open(fileName)
-            # puts file
-            while (not file.eof?)
-                buffer = file.read(256)
-                # puts buffer
+
+            if header.include?('POST')
+
+                params = JSON.parse(body)
+
+                user_data = "<li>name: #{params['viking']['name']}</li>\n<li>e-mail: #{params['viking']['email']}</li>"
+
+                client.puts "POST /thanks.html HTTP/1.0 200 OK\n#{Time.now.ctime}\nContent-Length: #{fileName.size}\r\n\r\n" \
+                    "#{file.read.gsub("<%= yield %>", user_data)}"
+
+            elsif header.include?('GET')
+                client.puts "GET /index.html HTTP/1.0 200 OK\n#{Time.now.ctime}\nContent-Length: #{fileName.size}\r\n\r\n#{file.read}"
             end
 
         else
             puts "HTTP/1.1 404/Object Not Found\r\nServer: ABC-server\r\n\r\n"
         end
 
-    end
-
-    # client.puts(Time.now.ctime)
-    # client.puts "Closing the connection."
     client.close
+
+    end
 }
