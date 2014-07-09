c5764fc34ae4bd41b370745767cc6df2050d45b1
mousterian <rivladi@gmail.com>
2014-07-08 17:36:47 -0700

    cleanup

--- Project2_7_Ruby_DataStructures/binarytree.rb
+++ Project2_7_Ruby_DataStructures/binarytree.rb
@@ -105,66 +105,21 @@ class TreeBuilder
 
     end
 
-    #Binary tree and array representation for the MaxHeap
-    def build_tree_from_sorted2(array)
-        j = 0
-        arr = []
-
-        array.each do |i|
-
-            left  = 2 * j + 1
-            right = 2 * j + 2
-            parent = array[j]# ((j) / 2).floor
-            value = array[j]
-
-            # puts value
-            tree = Node.new(i)
-
-            if (left) < array.length then
-                tree.leftchild = Node.new(array[left])
-                # arr << tree.leftchild
-            end
-
-            if (right ) < array.length then
-                tree.rightchild = Node.new(array[right])
-                # arr << tree.rightchild
-            end
-
-            arr << tree
-
-            j += 1
-
-        end
-
-        arr
-    end
-
-
     def tb_print(array)
         puts " "
-        # array.each {|i| puts "#{i.class} value: #{i.value} #{i.leftchild} #{i.rightchild}"}
-        # array.each {|i| puts "\n#{i.class} value #{i.value} left_value: #{i.leftchild.value if !i.leftchild.nil?} right_value: #{i.rightchild.value if i.rightchild.nil?}\n"}
         array.each {|i| puts "#{i} value #{i.value if !i.nil?} #{i.leftchild.value if !i.leftchild.nil?} #{i.rightchild.value if !i.rightchild.nil?}"}
-        # array.each {|i| puts "#{i.class} value #{i.value if !i.nil?}"}
-        array.each_with_index do |i,index|
-            if index > 0
-                # puts "#{i} value #{i.value if !i.nil?} #{i.leftchild.value if !i.leftchild.nil?} #{i.rightchild.value if !i.rightchild.nil?}"
-            end
-        end
-        puts array.length
-
     end
 end
 
-array_sorted = [16,14,10,8,7,9,3,2,4,1] #[10,20,30,40,50]
-# arr2 = [100,40,60,50,20]
-# puts "\n"
+
+# tests:
+array_sorted = [16,14,10,8,7,9,3,2,4,1]
 root = array_sorted[0]
-tb = TreeBuilder.new(root)
-node_arr = tb.build_tree(array_sorted)
+btree = TreeBuilder.new(root)
+btree_array = btree.build_tree(array_sorted)
 
-tb.tb_print(node_arr)
-puts tb.breadth_first_search(3).value
 
-puts tb.dfs_rec(8).value
+btree.tb_print(btree_array)
+puts btree.breadth_first_search(3)
+puts btree.dfs_rec(8)
 
