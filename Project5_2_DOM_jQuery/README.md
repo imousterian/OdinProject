cac96b79390fbf40aa5938a8d8cc70af9c12d453
mousterian <rivladi@gmail.com>
2014-09-17 14:22:47 -0700

    added project files

---
+++ Project5_2_DOM_jQuery/README.md
@@ -0,0 +1,3 @@
+An exercise in manipulating DOM with jQuery following [The Odin Project](http://www.theodinproject.com/javascript-and-jquery/manipulating-the-dom-with-jquery).
+
+CSS styling comes from this [tutorial](http://inspirationalpixels.com/tutorials/creating-tabs-with-html-css-and-jquery#step-css).
---
+++ Project5_2_DOM_jQuery/js/restaurant.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    $('#content').append("<div class='tabs'></div>").prepend("<img id='theImg' src='images/guinea-pig-1.jpg' />").prepend("<h1>We are the best restaurant for your guinea pig!</h1>");
+    $('.tabs').append("<ul class='tab-links'></ul>");
+    var lis = "<li><a href='#' id='atab1' class='tab1'>About Us</a></li>" +
+                "<li><a href='#' id='atab2' class='tab2'>Contact</a></li>" +
+                "<li><a href='#' id='atab3' class='tab3'>Menu</a></li>";
+    $('.tab-links').append(lis);
+    $('.tabs').append("<div class='tab-content'></div>");
+
+
+    var about_us = "<div id='tab1' class='tab tab1'><p>We are awesome!</p></div>";
+    var contact_us = "<div id='tab2' class='tab tab2'><p>Contact us!</p></div>";
+    var menu = "<div id='tab3' class='tab tab3'><p>Menu is filled with veggies.</p></div>";
+
+    replaceTabs('.tab1', about_us);
+    replaceTabs('.tab2', contact_us);
+    replaceTabs('.tab3', menu);
+
+});
+
+var replaceTabs = function(element, contents){
+    $(element).click(function(){
+        $('.active').remove();
+        $('.tab-content').html(contents);
+        var classy = "div." + $(this).attr('class');
+        $(classy).addClass('active');
+    });
+}
