36bc92925b78c267b3a83941b19ec4c6edd3d9d0
mousterian <rivladi@gmail.com>
2014-11-03 23:34:49 -0800

    major refactoring

---
+++ Project2_9_Final_Ruby_Chess/lib/converter.rb
@@ -0,0 +1,23 @@
+module Converter
+
+    def Converter.rows
+        return 8
+    end
+
+    def Converter.cols
+        return 8
+    end
+
+    def Converter.convert_x_y_to_location(x,y)
+        x * rows + y + 1
+    end
+
+    def Converter.convert_location_to_x_y(loc)
+        (0...rows).each do |i|
+            (0...cols).each do |j|
+                return i, j if loc == (i * rows + j + 1).to_s
+            end
+        end
+        nil
+    end
+end
--- Project2_9_Final_Ruby_Chess/lib/piece.rb
+++ Project2_9_Final_Ruby_Chess/lib/piece.rb
@@ -1,66 +1,37 @@
+require './lib/converter.rb'
+
 class Piece
 
-    attr_accessor :id, :x, :y, :old_location, :color
+    include Converter
+    attr_accessor :id, :x, :y
 
     def initialize(color=:white,x=0,y=0)
         @color = color
-        @x = x
-        @y = y
-        update_old_location
-        update_id(x,y)
+        @x, @y = x, y
+        set_id
+    end
+
+    def color
+        @color
     end
 
     def description
         @color == :white ? 'w' : 'b'
     end
 
-    def update_old_location
-        @old_location = [@x, @y]
-    end
-
-    def update_id(x,y)
-        @id = x * 8 + y + 1
+    def set_id
+        @id = @x * 8 + @y + 1
     end
 
     def move_to(x,y)
         if x.nil?
             raise ArgumentError, "x and y must be within the board"
         else
-            update_id(x,y)
-            update_old_location
             @x, @y = x, y
+            set_id
         end
     end
 
-    def is_it_blocked?(board, from_x, from_y, to_x, to_y)
-
-        result = false
-        direction_x = from_x > to_x ? 1 : -1
-        direction_y = from_y > to_y ? 1 : -1
-
-        row = (from_x - to_x).abs
-        col = (from_y - to_y).abs
-
-        if  row > col
-            diff = row
-        else
-            diff = col
-        end
-
-        (1...diff).each do |i|
-            if row > col
-                cell = board.board[to_x+i*direction_x][to_y]
-                result = true if !cell.nil?
-            elsif col > row
-                cell = board.board[to_x][to_y+i*direction_y]
-                result = true if !cell.nil?
-            else
-                cell = board.board[to_x+i*direction_x][to_y+i*direction_y]
-                result = true if !cell.nil?
-            end
-        end
-        result
-    end
 end
 
 class Pawn < Piece
@@ -70,44 +41,51 @@ class Pawn < Piece
         @step_counter = 2
     end
 
-    def decrement_counter
-        @step_counter -= 1
-    end
-
     def description
         'P' + super()
     end
 
-    def where_to_go?(x,y)
+    def decrement_step_counter
+        @step_counter -= 1
+    end
+
+    def determine_step_counter(to_x)
+        step = 0
         if @step_counter > 1
-            if (@old_location[0] - x).abs == 1
+            if (@x - to_x).abs == 1
                 step = 1
-            elsif (@old_location[0] - x).abs == 2
+            elsif (@x - to_x).abs == 2
                 step = 2
-            else
-                step = 0
             end
         else
             step = 1
         end
-        if @color == :white
-            x == (@x - step) && (y == @y) ? true : false
+        return step
+    end
+
+    def where_to_go?(to_x,to_y)
+
+        step = determine_step_counter(to_x)
+
+        if  @color == :white
+            to_x == (@x - step) && (to_y == @y) ? true : false
         else
-            x == (@x + step) && (y == @y) ? true : false
+            to_x == (@x + step) && (to_y == @y) ? true : false
         end
     end
 
     def valid_move?(board, from_x, from_y, to_x, to_y)
-        destination = board.board[to_x][to_y]
+        destination = board.destination
         x = (from_x - to_x).abs
         y = (from_y - to_y).abs
+
         return true if destination.nil? && where_to_go?(to_x, to_y)
+
         if from_x != to_x && from_y != to_y && x <= 1 && y <= 1
             return true if !destination.nil? && destination.color != @color
         end
         false
     end
-
 end
 
 class King < Piece
@@ -123,27 +101,31 @@ class King < Piece
            move_x = i[0]
            move_y = i[1]
            if (move_x >= 0 && move_x <= 7) && (move_y >= 0 && move_y <= 7)
-               cell = board.board[move_x][move_y]
+                loc = Converter.convert_x_y_to_location(move_x, move_y)
+                cell = board.find_piece_in_collection(loc)
                 if cell.nil?
                     places << [move_x, move_y]
                 else
-                    if cell.color != @color
-                        places << [move_x, move_y]
-                    end
+                    places << [move_x, move_y] if cell.color != @color
                 end
             end
         end
         places
     end
 
-    def valid_move?(board, from_x, from_y, to_x, to_y)
-        destination = board.board[to_x][to_y]
-        block = is_it_blocked?(board, from_x, from_y, to_x, to_y)
-
+    def piece_movements?(from_x, from_y, to_x, to_y)
         x = (from_x - to_x).abs
         y = (from_y - to_y).abs
-        if (x >= 0 and x <= 1 && y >= 0 && y <= 1)
-            return true if destination.nil? || destination.color != @color
+        return true if (x >= 0 and x <= 1 && y >= 0 && y <= 1)
+        false
+    end
+
+    def valid_move?(board, from_x, from_y, to_x, to_y)
+        destination = board.destination
+        move = piece_movements?(from_x, from_y, to_x, to_y)
+
+        if destination.nil? || destination.color != @color
+            return true if move == true
         end
         false
     end
@@ -155,25 +137,21 @@ class Rook < Piece
         'R' + super()
     end
 
-    def rook_move?(from_x, from_y, to_x, to_y)
+    def piece_movements?(from_x, from_y, to_x, to_y)
         return true if from_x == to_x || from_y == to_y
-        return true if (from_x-to_x).abs == (from_y - to_y).abs
         false
     end
 
     def valid_move?(board, from_x, from_y, to_x, to_y)
-        destination = board.board[to_x][to_y]
-        move = rook_move?(from_x, from_y, to_x, to_y)
-        block = is_it_blocked?(board, from_x, from_y, to_x, to_y)
+        destination = board.destination
+        move = piece_movements?(from_x, from_y, to_x, to_y)
+        path_blocked = board.is_it_blocked?(from_x, from_y, to_x, to_y)
 
-        if !destination.nil?
-            if destination.color != @color
-                return true if move == true && block == false
-            end
-        else
-            return true if move == true && block == false
+        if destination.nil? || destination.color != @color
+            return true if move == true && path_blocked == false
         end
     end
+
 end
 
 class Knight < Piece
@@ -182,14 +160,18 @@ class Knight < Piece
         'k' + super()
     end
 
-    def valid_move?(board, from_x, from_y, to_x, to_y)
-        destination = board.board[to_x][to_y]
+    def piece_movements?(from_x, from_y, to_x, to_y)
         x = (from_x - to_x).abs
         y = (from_y - to_y).abs
+        return true if (x * 2 == 4 && y == 1) || (x * 2 == 2 && y == 2)
+        false
+    end
+
+    def valid_move?(board, from_x, from_y, to_x, to_y)
+        destination = board.destination
+        move = piece_movements?(from_x, from_y, to_x, to_y)
         if destination.nil? || destination.color != @color
-            if (x * 2 == 4 && y == 1) || (x * 2 == 2 && y == 2)
-                return true
-            end
+            return move
         end
         false
     end
@@ -200,23 +182,18 @@ class Bishop < Piece
         'B' + super()
     end
 
-    def bishop_move?(from_x, from_y, to_x, to_y)
-        return false if from_x == to_x || from_y == to_y
+    def piece_movements?(from_x, from_y, to_x, to_y)
         return true if (from_x-to_x).abs == (from_y - to_y).abs
         false
     end
 
     def valid_move?(board, from_x, from_y, to_x, to_y)
-        destination = board.board[to_x][to_y]
-        move = bishop_move?(from_x, from_y, to_x, to_y)
-        block = is_it_blocked?(board, from_x, from_y, to_x, to_y)
+        destination = board.destination
+        move = piece_movements?(from_x, from_y, to_x, to_y)
+        path_blocked = board.is_it_blocked?(from_x, from_y, to_x, to_y)
 
-        if !destination.nil?
-            if destination.color != @color
-                return true if move == true && block == false
-            end
-        else
-            return true if move == true && block == false
+        if destination.nil? || destination.color != @color
+            return true if move == true && path_blocked == false
         end
     end
 end
@@ -228,17 +205,11 @@ class Queen < Piece
     end
 
     def valid_move?(board, from_x, from_y, to_x, to_y)
+        destination = board.destination
+        path_blocked = board.is_it_blocked?(from_x, from_y, to_x, to_y)
 
-        destination = board.board[to_x][to_y]
-        move = true
-        block = is_it_blocked?(board, from_x, from_y, to_x, to_y)
-
-        if !destination.nil?
-            if destination.color != @color
-                return true if move == true && block == false
-            end
-        else
-            return true if move == true && block == false
+        if destination.nil? || destination.color != @color
+            return true if path_blocked == false
         end
     end
 end
